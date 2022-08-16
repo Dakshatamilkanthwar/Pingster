58c3feb2b6bb8e2829511e8465dae7d22f8aef94
Dakshatamilkanthwar <67453062+Dakshatamilkanthwar@users.noreply.github.com>
2022-08-16 14:50:21 +0530

    Add files via upload

---
+++ Assign.py
@@ -0,0 +1,10 @@
+import os
+os.system('cls')
+print('#'* 60)
+IP=input("Enter IP address to check for reachability::")
+os.system('ping {}'.format(IP))
+os.system('ping {}'.format(hostname))
+print('-'* 60)
+print("press any key to get exit")
+
+~                           
